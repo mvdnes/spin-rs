481c91392c51c5f7d0697030f4b7a4567e97c791
M Farkas-Dyck <strake888@gmail.com>
2018-07-20 19:35:01 -0800

    use `core::sync::atomic::spin_loop_hint`

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@
 
 //! Synchronization primitives based on spinning
 
-#![cfg_attr(feature = "asm", feature(asm))]
 #![cfg_attr(feature = "core_intrinsics", feature(core_intrinsics))]
 #![cfg_attr(feature = "const_fn", feature(const_fn, const_unsafe_cell_new, const_atomic_usize_new))]
 
@@ -24,5 +23,3 @@ mod rw_lock;
 
 #[cfg(feature = "once")]
 mod once;
-
-mod util;
