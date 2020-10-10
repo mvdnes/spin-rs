07397e9d4da02bfeefe3c8ecfa08b0fc72dc154c
Joshua Barretto <joshua.s.barretto@gmail.com>
2020-10-10 14:31:27 +0100

    Fixed broken README link

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 #![deny(missing_docs)]
 
-//! This crate provides spin-based versions of the primitives in `std::sync`. Because synchronization is done
-//! through spinning, the primitives are suitable for use in `no_std` environments.
+//! This crate provides [spin-based](https://en.wikipedia.org/wiki/Spinlock) versions of the primitives in
+//! `std::sync`. Because synchronization is done through spinning, the primitives are suitable for use in
+//! `no_std` environments.
 //!
 //! # Features
 //!
