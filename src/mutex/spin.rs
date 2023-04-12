0aa47606e22007d3b785738fa53acf2aaaf4c2a7
Klim Tsoutsman <klim@tsoutsman.com>
2023-04-12 22:20:08 +1000

    Expose weak versions of CAS methods
    Useful when using `spin` to implement higher-level sync primitives.
    
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- src/mutex/spin.rs
+++ src/mutex/spin.rs
@@ -177,20 +177,14 @@ impl<T: ?Sized, R: RelaxStrategy> SpinMutex<T, R> {
     pub fn lock(&self) -> SpinMutexGuard<T> {
         // Can fail to lock even if the spinlock is not locked. May be more efficient than `try_lock`
         // when called in a loop.
-        while self
-            .lock
-            .compare_exchange_weak(false, true, Ordering::Acquire, Ordering::Relaxed)
-            .is_err()
-        {
-            // Wait until the lock looks unlocked before retrying
+        loop {
+            if let Some(guard) = self.try_lock_weak() {
+                return guard;
+            }
+
             while self.is_locked() {
                 R::relax();
             }
-        }
-
-        SpinMutexGuard {
-            lock: &self.lock,
-            data: unsafe { &mut *self.data.get() },
         }
     }
 }
@@ -237,10 +231,34 @@ impl<T: ?Sized, R> SpinMutex<T, R> {
     pub fn try_lock(&self) -> Option<SpinMutexGuard<T>> {
         // The reason for using a strong compare_exchange is explained here:
         // https://github.com/Amanieu/parking_lot/pull/207#issuecomment-575869107
-        if self
-            .lock
-            .compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed)
-            .is_ok()
+        self.try_lock_internal(true)
+    }
+
+    /// Try to lock this [`SpinMutex`], returning a lock guard if succesful.
+    ///
+    /// Unlike [`SpinMutex::try_lock`], this function is allowed to spuriously fail even when the mutex is unlocked,
+    /// which can result in more efficient code on some platforms.
+    #[inline(always)]
+    pub fn try_lock_weak(&self) -> Option<SpinMutexGuard<T>> {
+        self.try_lock_internal(false)
+    }
+
+    #[inline(always)]
+    fn try_lock_internal(&self, strong: bool) -> Option<SpinMutexGuard<T>> {
+        let f = if strong {
+            AtomicBool::compare_exchange
+        } else {
+            AtomicBool::compare_exchange_weak
+        };
+
+        if f(
+            &self.lock,
+            false,
+            true,
+            Ordering::Acquire,
+            Ordering::Relaxed,
+        )
+        .is_ok()
         {
             Some(SpinMutexGuard {
                 lock: &self.lock,
